a2cd5b315cc1c5918545b7121f5a50719cb1b443
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-07-25 19:53:57 +0000

    ntro to SQLAlchemy

--- src/models.py
+++ src/models.py
@@ -8,26 +8,40 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    #roles_id = Column(Integer, ForeignKey('roles.id'), nullable=False)
+    #role = relationship('Role', uselist=False) # [<Role 1>] # Devolver Objecto directamente: uselist=False <Role 1>
+    roles = relationship('Role', secondary="roles_users") # [<Role 1>, <Role 2>]
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def get_user_full_info(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "role": {
+                "id": self.role.id,
+                "name": self.role.name
+            },
+            "role": "http://api.com/api/role/" + self.role.id 
+        }
+
+class RoleUser(Base):
+    __tablename__ = 'roles_users'
+    roles_id = Column(Integer, ForeignKey('roles.id'), primary_key=True) 
+    users_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+
+
+class Role(Base):
+    __tablename__ = 'roles'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nane = Column(String(100), unique=True, nullable=False)
+    #users = relationship('User', backref='role') # [<User 1>, <User 2>]
+    users = relationship('User', secondary="roles_users") # [<User 1>, <User 2>]
 
-    def to_dict(self):
-        return {}
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
